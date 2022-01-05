3e02bc70f44eae3d5e1697bfc01d675b7f30d5f2
PC <double.happiness@outlook.com>
2022-01-05 13:38:16 +0000

    added change

---
+++ csv.js
@@ -0,0 +1,136 @@
+const allRows = [];
+
+async function fileToLines(file) {
+  return new Promise((resolve, reject) => {
+    reader = new FileReader();
+    reader.onload = function(e) {
+      parsedLines = e.target.result.split(/\r|\n|\r\n/);
+      resolve(parsedLines);
+    };
+    reader.readAsText(file);
+  });
+}
+
+document
+.getElementById('fileInput')
+.addEventListener('change', async function(e) {
+  var file = e.target.files[0];
+
+  if (file != undefined) {
+    fileToLines(file).then( async id => {
+      console.log(id)
+      console.log(parsedLines)
+      console.log(typeof id);
+
+      var idInt = id.map(Number);
+      var idFiltered = id.filter(function(v){return v!==''});
+
+      console.log(idFiltered)
+
+      for(let id of idFiltered) {
+        const row = await getRelease(id);
+        allRows.push(row);
+      }
+      download();
+    });
+  }
+});
+
+function getRelease(idFiltered) {
+  return fetch(`https://api.discogs.com/releases/${idFiltered}`, {
+    headers: {
+    'User-Agent': 'Dispodger/0.1',
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.message === 'Release not found.') {
+      return { error: `Release with ID ${idFiltered} does not exist` };
+    } else {
+      const id = data.id;
+      const delimiter = document.getElementById("delimiter").value || "|";
+      const artists = data.artists ? data.artists.map(artist => artist.name) : [];
+      const barcode = data.identifiers.filter(id => id.type === 'Barcode')
+      .map(barcode => barcode.value);
+      var formattedBarcode = barcode.join(delimiter);
+      const country = data.country || 'Unknown';
+      const genres = data.genres || [];
+      const formattedGenres = genres.join(delimiter);
+      const labels = data.labels ? data.labels.map(label => label.name) : [];
+      const formattedLabels = labels.join(delimiter);
+      const catno = data.labels ? data.labels.map(catno => catno.catno) : [];
+      const formattedCatNo = catno.join(delimiter);
+      const styles = data.styles || [];
+      const formattedStyles = styles.join(delimiter);
+      const tracklist = data.tracklist ? data.tracklist
+      .map(track => track.title) : [];
+      const formattedTracklist = tracklist.join(delimiter);
+      const year = data.year || 'Unknown';
+      const format = data.formats ? data.formats.map(format => format.name) : [];
+      const qty = data.formats ? data.formats.map(format => format.qty) : [];
+      const descriptions = data.formats ? data.formats
+      .map(descriptions => descriptions.descriptions) : [];
+      const preformattedDescriptions = descriptions.toString()
+      .replace('"','""').replace(/,/g, ', ');
+      const formattedDescriptions = '"' + preformattedDescriptions + '"';
+      console.log(idFiltered,
+        artists,
+        format,
+        qty,
+        formattedDescriptions,
+        formattedLabels,
+        formattedCatNo,
+        country,
+        year,
+        formattedGenres,
+        formattedStyles,
+        formattedBarcode,
+        formattedTracklist
+      )
+
+      return [idFiltered,
+        artists,
+        format,
+        qty,
+        formattedDescriptions,
+        formattedLabels,
+        formattedCatNo,
+        country,
+        year,
+        formattedGenres,
+        formattedStyles,
+        formattedBarcode,
+        formattedTracklist
+      ];
+    }
+  });
+}
+
+function download() {
+  const ROW_NAMES = [
+    "release_id",
+    "artist",
+    "format",
+    "qty",
+    "format descriptions",
+    "label",
+    "catno",
+    "country",
+    "year",
+    "genres",
+    "styles",
+    "barcode",
+    "tracklist"
+  ];
+  var csvContent = "data:text/csv;charset=utf-8,"
+  + ROW_NAMES + "\n" + allRows.map(e => e.join(",")).join("\n");
+
+  console.log(csvContent);
+
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "my_data.csv");
+  document.body.appendChild(link); // Required for FF
+  link.click();
+}
