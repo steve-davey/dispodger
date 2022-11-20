0d3d0d3c576f0a05c58850d0c510cc85e6a3fab4
double-happ1ness <double.happiness@outlook.com>
2022-11-20 11:31:08 +0000

    npm install throttled-queue

--- csv.js
+++ csv.js
@@ -1,3 +1,4 @@
+// import throttledQueue from 'throttled-queue';
 let allRows = [];
 
 async function fileToLines(file) {
@@ -18,15 +19,7 @@ document
 
     if (file != undefined) {
       fileToLines(file).then(async id => {
-        console.log(id)
-        console.log(parsedLines)
-        console.log(typeof id);
-
-        var idInt = id.map(Number);
         var idFiltered = id.filter(function (v) { return v !== '' });
-
-        console.log(idFiltered)
-
         if (file != undefined) {
           allRows = [];
         }
@@ -40,29 +33,13 @@ document
     }
   });
 
-  function debounce(inner, ms = 0) {
-    let timer = null;
-    let resolves = [];
-  
-    return function (...args) {
-      // Run the function after a certain amount of time
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        /* Get the result of the inner function, then apply it to the resolve function of
-        each promise that has been created since the last time the inner function was run */
-        let result = inner(...args);
-        resolves.forEach(r => r(result));
-        resolves = [];
-      }, ms);
-  
-      return new Promise(r => resolves.push(r));
-    };
-  }
-  
-  const throttleFetch = debounce(fetch, 2500);
+const throttledQueue = require('throttled-queue');
+const throttle = throttledQueue(1, 1000);
 
-function getRelease(idFiltered) {
-  return throttleFetch(`https://api.discogs.com/releases/${idFiltered}`, {
+throttle(() => getRelease(idFiltered));
+
+async function getRelease(idFiltered) {
+  return fetch(`https://api.discogs.com/releases/${idFiltered}`, {
     headers: {
       'User-Agent': 'Dispodger/0.1',
       'Authorization': 'Discogs key=dANvHNaHmlAtGVZVZPhq, secret=KOuEKidlWuHjlsNKPBHJOrseESdNJqhW',
@@ -72,46 +49,26 @@ function getRelease(idFiltered) {
       if (data.message === 'Release not found.') {
         return { error: `Release with ID ${idFiltered} does not exist` };
       } else {
-        const id = data.id;
-        const delimiter = document.getElementById("delimiter").value || "|";
-        const artists = data.artists ? data.artists.map(artist => artist.name) : [];
-        const barcode = data.identifiers.filter(id => id.type === 'Barcode')
-          .map(barcode => barcode.value);
-        var formattedBarcode = barcode.join(delimiter);
-        const country = data.country || 'Unknown';
-        const genres = data.genres || [];
-        const formattedGenres = genres.join(delimiter);
-        const labels = data.labels ? data.labels.map(label => label.name) : [];
-        const formattedLabels = labels.join(delimiter);
-        const catno = data.labels ? data.labels.map(catno => catno.catno) : [];
+        const { country = 'Unknown', genres = [], styles = [], year = 'Unknown' } = data;
+        const artists = data.artists?.map?.(artist => artist.name);
+        const barcode = data.identifiers.filter(id => id.type === 'Barcode').map(barcode => barcode.value);
+        const catno = data.labels.map(catno => catno.catno);
+        const descriptions = data.formats.map(descriptions => descriptions.descriptions);
+        const format = data.formats.map(format => format.name);
+        const labels = data.labels.map(label => label.name);
+        const qty = data.formats.map(format => format.qty);
+        const tracklist = data.tracklist.map(track => track.title);
+        const formattedLabels = labels.map(label => label.name);
+        const delimiter = document.getElementById("delimiter").value ?? "|";
+        const formattedBarcode = barcode.join(delimiter);
         const formattedCatNo = catno.join(delimiter);
-        const styles = data.styles || [];
+        const formattedGenres = genres.join(delimiter);
         const formattedStyles = styles.join(delimiter);
-        const tracklist = data.tracklist ? data.tracklist
-          .map(track => track.title) : [];
         const formattedTracklist = tracklist.join(delimiter);
-        const year = data.year || 'Unknown';
-        const format = data.formats ? data.formats.map(format => format.name) : [];
-        const qty = data.formats ? data.formats.map(format => format.qty) : [];
-        const descriptions = data.formats ? data.formats
-          .map(descriptions => descriptions.descriptions) : [];
         const preformattedDescriptions = descriptions.toString()
           .replace('"', '""').replace(/,/g, ', ');
         const formattedDescriptions = '"' + preformattedDescriptions + '"';
-        console.log(idFiltered,
-          artists,
-          format,
-          qty,
-          formattedDescriptions,
-          formattedLabels,
-          formattedCatNo,
-          country,
-          year,
-          formattedGenres,
-          formattedStyles,
-          formattedBarcode,
-          formattedTracklist
-        )
+        console.log(data);
 
         return [idFiltered,
           artists,
@@ -147,11 +104,11 @@ function download() {
     "barcode",
     "tracklist"
   ];
-  var csvContent = "data:text/csv;charset=utf-8,"
+  const csvContent = "data:text/csv;charset=utf-8,"
     + ROW_NAMES + "\n" + allRows.map(e => e.join(",")).join("\n");
 
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement("a");
+  const encodedUri = encodeURI(csvContent);
+  const link = document.createElement("a");
   link.setAttribute("href", encodedUri);
   link.setAttribute("download", "my_data.csv");
   document.body.appendChild(link); // Required for Firefox
